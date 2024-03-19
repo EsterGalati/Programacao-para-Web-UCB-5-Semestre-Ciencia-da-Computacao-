746ea78d2f352105a5ddbdd7b9f619ef31f92ba4
ESTER <111468977+EsterGalati@users.noreply.github.com>
2024-03-19 19:30:36 -0300

    Add files via upload

---
+++ Aulajs0703/numeros.js
@@ -0,0 +1,12 @@
+var i;
+for(i=0; i<4; i++){
+var n =Number(prompt("Digite um numero"));
+
+if(n % 2 == 0){
+   // document.write("Numero Par "+n); 
+    alert ("Numero Par "+n); 
+}else{
+    alert("Numero Impar "+n);   
+}
+}
+
